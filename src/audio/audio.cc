b35954f405e73f68bb9bd53615b207f8e20d6621
Jim <jimsamuel50@gmail.com>
2024-11-11 20:06:04 +0100

    Media enhancement on audio & video . Rfs

--- src/audio/audio.cc
+++ src/audio/audio.cc
@@ -1,46 +1,50 @@
 #include "audio.hh"
 
-template <typename ... Ts> 
-void process_audio_frames(Ts ... ts){
+template <typename... Ts>
+void process_audio_frames(Ts... ts)
+{
     (read_audio_frames(ts), ...)
 }
 
-/* reads incoming stream and fetches the audio data */
-template <typename ... Ts>
-void Audio<Ts ...>::read_audio_frames(const char* audiofile) {
-    AVFormatContext* context = nullptr;
-    if(avformat_open_input(&context, audiofile,nullptr, nullptr) < 0)
+template <typename... Ts>
+void Audio<Ts...>::read_audio_frames(const char *audiofile)
+{
+    AVFormatContext *context = nullptr;
+    if (avformat_open_input(&context, audiofile, nullptr, nullptr) < 0)
         audio_encoding_error("error encoding audiofile");
-    if(avformat_find_stream_info(context, nullptr) < 0)
+    if (avformat_find_stream_info(context, nullptr) < 0)
         audio_encoding_error("cannot find stream info");
     int audio_file_stream_index = audio_stream_index;
 
-    /* read the number of incoming streams from the context */
-    /* checks the codec parameters to fetch the codec type */
-    for(int i = 0; i < context->nb_streams; i++){
-        if(context->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO)
-            audio_file_stream_index  = i; break;
+    for (int i = 0; i < context->nb_streams; i++)
+    {
+        if (context->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO)
+            audio_file_stream_index = i;
+        break;
     }
-    if(audio_file_stream_index == -1) audio_encoding_error("invalid audio file");
+    if (audio_file_stream_index == -1)
+        audio_encoding_error("invalid audio file");
 
-    /* operations to get the codec and codec parameters */
-    AVCodecParameters* parameters = context->streams[audio_file_stream_index]->codecpar;
-    const AVCodec* avcodec = avcodec_find_decoder(parameters->codec_id);
-    AVCodecContext* avcontext = avcodec_alloc_context3(avcodec);
-    avcodec_parameters_to_context(avcontext,parameters);
+    AVCodecParameters *parameters = context->streams[audio_file_stream_index]->codecpar;
+    const AVCodec *avcodec = avcodec_find_decoder(parameters->codec_id);
+    AVCodecContext *avcontext = avcodec_alloc_context3(avcodec);
+    avcodec_parameters_to_context(avcontext, parameters);
 
-    /* memory allocation for the frames and packets */
-    AVFrame* frames = av_frame_alloc(); AVPacket* packet;
+    AVFrame *frames = av_frame_alloc();
+    AVPacket *packet;
     av_init_packet(packet);
-    while(av_read_frame(context,packet) < 0) {
-        /* match the packet stream index to the video stream index */
-        /* if value matches send packet and recieve frame data */
-        if(packet->stream_index == audio_file_stream_index) {
+    while (av_read_frame(context, packet) < 0)
+    {
+        if (packet->stream_index == audio_file_stream_index)
+        {
             int ret = avcodec_send_packet(avcontext, packet);
-            if(ret < 0) audio_encoding_error("error sending packet");
-            while(ret >= 0) {
+            if (ret < 0)
+                audio_encoding_error("error sending packet");
+            while (ret >= 0)
+            {
                 ret = avcodec_receive_frame(avcontext, frames);
-                if(ret == AVERROR(EAGAIN) || AVERROR(EOF)) break;
+                if (ret == AVERROR(EAGAIN) || AVERROR(EOF))
+                    break;
             }
         }
     }
@@ -50,7 +54,8 @@ void Audio<Ts ...>::read_audio_frames(const char* audiofile) {
     avformat_free_context(context);
 }
 
-void audio_encoding_error(char* errormsg){
+void audio_encoding_error(char *errormsg)
+{
     perror(errormsg);
     std::cout << std::endl;
 }
--- src/video/video.cc
+++ src/video/video.cc
@@ -1,48 +1,52 @@
 #include "video.hh"
 
-template <typename ... Ts>
-void Video<Ts ...>::process_video_frames(Ts ... ts) {
+template <typename... Ts>
+void Video<Ts...>::process_video_frames(Ts... ts)
+{
     (read_video_frames(ts), ...);
 }
 
-/* reads incoming stream and fetches the video data */
-template <typename ... Ts>
-void Video<Ts ...>::read_video_frames(const char* filename) {
-    AVFormatContext* context = nullptr;
-    if(avformat_open_input(&context, filename, nullptr, nullptr) < 0)
+template <typename... Ts>
+void Video<Ts...>::read_video_frames(const char *filename)
+{
+    AVFormatContext *context = nullptr;
+    if (avformat_open_input(&context, filename, nullptr, nullptr) < 0)
         video_encoding_error("error opening file");
-    if(avformat_find_stream_info(context, nullptr) < 0)
+    if (avformat_find_stream_info(context, nullptr) < 0)
         video_encoding_error("cannot find video context");
     int video_stream_index_value = video_stream_index;
-    
-    /* read the number of incoming streams from the context */
-    /* checks the codec parameters to fetch the codec type */
-    for(int i = 0; i < context->nb_streams;i++){
-        if(context->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
-            video_stream_index_value = i; break;
-    }
-    if(video_stream_index_value == -1) video_encoding_error("invalid video stream");
 
-    /* operations to get the codec and codec parameters */
-    AVCodecParameters* parameters = context->streams[video_stream_index_value]->codecpar;
-    const AVCodec* avcodec = avcodec_find_decoder(parameters->codec_id);
-    AVCodecContext* avcontext = avcodec_alloc_context3(avcodec);
-    avcodec_parameters_to_context(avcontext,parameters);
-    if(avcodec_open2(avcontext,avcodec, nullptr) < 0) 
+    for (int i = 0; i < context->nb_streams; i++)
+    {
+        if (context->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
+            video_stream_index_value = i;
+        break;
+    }
+    if (video_stream_index_value == -1)
+        video_encoding_error("invalid video stream");
+
+    AVCodecParameters *parameters = context->streams[video_stream_index_value]->codecpar;
+    const AVCodec *avcodec = avcodec_find_decoder(parameters->codec_id);
+    AVCodecContext *avcontext = avcodec_alloc_context3(avcodec);
+    avcodec_parameters_to_context(avcontext, parameters);
+    if (avcodec_open2(avcontext, avcodec, nullptr) < 0)
         video_encoding_error("cannot open context");
-    
-    /* memory allocation for the frames and packets */
-    AVFrame* frame = av_frame_alloc(); AVPacket* packet;
+
+    AVFrame *frame = av_frame_alloc();
+    AVPacket *packet;
     av_init_packet(packet);
-    while(av_read_frame(context,packet) < 0 ){
-        /* match the packet stream index to the video stream index */
-        /* if value matches send packet and recieve frame data */
-        if(packet->stream_index == video_stream_index_value) {
-            int ret = avcodec_send_packet(avcontext,packet);
-            if (ret < 0) video_encoding_error("cannot send packet");
-            while(avcodec_receive_frame(avcontext, frame)){
+    while (av_read_frame(context, packet) < 0)
+    {
+        if (packet->stream_index == video_stream_index_value)
+        {
+            int ret = avcodec_send_packet(avcontext, packet);
+            if (ret < 0)
+                video_encoding_error("cannot send packet");
+            while (avcodec_receive_frame(avcontext, frame))
+            {
                 ret = avcodec_receive_frame(avcontext, frame);
-                if(ret == AVERROR(EAGAIN) || ret == AVERROR(EOF)) break;
+                if (ret == AVERROR(EAGAIN) || ret == AVERROR(EOF))
+                    break;
             }
         }
     }
@@ -52,7 +56,8 @@ void Video<Ts ...>::read_video_frames(const char* filename) {
     avformat_free_context(context);
 }
 
-void video_encoding_error(char* errormsg) {
+void video_encoding_error(char *errormsg)
+{
     perror(errormsg);
     std::cout << std::endl;
 }
